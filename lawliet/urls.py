db74e114d3156c2ff43c24040302b1173247ce59
jafaralimh <jafarhabib77@gmail.com>
2016-12-26 18:41:54 +0700

    I'm the one with Python. Python is with me.

--- lawliet/urls.py
+++ lawliet/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.conf.urls import include
+from django.contrib.auth import views
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('webapp.urls')),
 ]
